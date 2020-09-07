c5e632e32ca215b61f3fcc0475aa458022e08753
XDfelek <feleksteam@wp.pl>
2020-09-07 19:32:59 +0200

    zadania test git

--- Podstawy10/src/WyrazeniaRegularne.java
+++ Podstawy10/src/WyrazeniaRegularne.java
@@ -1,2 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WyrazeniaRegularne {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile(".*");
+        Matcher matcher = pattern.matcher("Kamil");
+
+        //zwraca true jak jakikolwiek kawalek pasuje do pattern
+        boolean textContainsPattern = matcher.find();
+        System.out.println("textContainsPattern = " + textContainsPattern);
+
+        //zwraca true jak CALY tekst pasuje do pattern
+        boolean matches = matcher.matches();
+        System.out.println("matches = " + matches);
+    }
 }
